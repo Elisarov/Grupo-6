868bf00504ec5baafe8aed7f4a8a511c30067850
Elisarov <92781926+Elisarov@users.noreply.github.com>
2021-11-04 21:30:44 +0100

    Add files via upload

---
+++ Progetto_Elisa.R
@@ -0,0 +1,96 @@
+source("teoriadecision_funciones_incertidumbre.R")
+
+alfas = function(tablaX, precisionAlfa=0.05, favorable=TRUE){
+  X = tablaX;
+  Altmin = apply(X,MARGIN=1,min);
+  Altmax = apply(X,MARGIN=1,max);
+  valfa = seq(from=0,to=1,by=precisionAlfa);
+  vHurwicz = rep(0,length(valfa));
+  alternativa = rep(0,length(valfa));
+  Alt_vHurwicz = rep(0,length(valfa));
+  for (i in 1:length(valfa)) {
+    alfab = valfa[i];
+    if (favorable) {
+      vAltH = alfab * Altmax + (1-alfab) * Altmin;
+      vHurwicz[i] = max(vAltH)
+      alternativa[i]=which.max(vAltH) #que alternativa lo cumple
+    } else {
+      vAltH = alfab * Altmin + (1-alfab) * Altmax;
+      vHurwicz[i] = min(vAltH)
+      alternativa[i]=which.min(vAltH) #que alternativa lo cumple
+    }
+  }
+  
+  altsinrep=unique(alternativa) #me quedo con las diferentes alternativas (quitando las repeticiones)
+  alfasb = (which(!duplicated(alternativa)))*precisionAlfa - precisionAlfa
+  #posiciones de cuándo aparecen las nuevas alternativas en el intervalo [0,1] (alfas)
+  alfas=alfasb[-1] #quitamos la primera que siempre será 0)
+  
+  #salida que se nos pide:
+  return(
+    
+    if (length(alfas)==1) {
+      cat ("La mejor alternativa es",altsinrep[1],"para alfa perteneciente a [ 0 ,",
+           alfas[1],"] y la mejor alternativa es",altsinrep[2],
+           "para alfa perteneciente a [",alfas[1],", 1 ]")
+    } else {
+      if (length(alfas)==2) {
+        cat ("La mejor alternativa es",altsinrep[1],"para alfa perteneciente a [ 0 ,",
+             alfas[1],"], la mejor alternativa es",altsinrep[2],
+             "para alfa perteneciente a [",alfas[1],",",alfas[2],
+             "] y la mejor alternativa es", altsinrep[3], "si alfa pertenece a [",
+             alfas[2],", 1 ]")
+      } else {
+        cat ("La mejor alternativa es",altsinrep[1],"para alfa perteneciente a [ 0 ,",
+             alfas[1],"], ")
+        for (i in 1:(length(alfas)-1)) {
+          cat ("la mejor alternativa es",altsinrep[i],"para alfa perteneciente a [",
+               alfas[i],",",alfas[i+1],"] ")
+        }
+        cat ("y la mejor alternativa es", altsinrep[length(altsinrep)], "si alfa pertenece a [",
+             alfas[length(alfas)],", 1 ]")
+      }
+      
+    })
+}
+
+Enunciado:
+María tiene que elegir un piso donde vivir por un año.
+Su decisión depiende da la posición del piso y da los costes que va a tener en base a la posición. 
+María puede elegir si vivir:
+En centro
+[0-10]km dal centro
++ 10 km
+
+Si elige el piso en el centro tiene que pagar 500??? de alquilar, 10??? de facturas y 0??? de transporte al mes
+
+Si elige el piso en cerca del centro [0-10]km  tiene que pagar 430??? de alquilar, 16??? de facturas y 20??? de transporte al mes
+
+Si elige el más lejos tiene que pagar 350??? de alquilar, 20??? de facturas y 70??? de transporte al mes
+
+¿Cuál es la mejor decisión para que se gaste lo menos posible?
+  
+Planteamiento:
+-Un decisor
+-Modelo no favorable
+
+Alternativas:
+1 = "Centro"
+2 = "[0-10]km desde el centro"
+3 = "+10 km desde el centro"
+
+Estados de la naturaleza:
+E1: alquilar
+E2: facturas
+E3: transporte 
+
+tE = matrix(c(500,430,350,12,9,20,0,20,70),nrow = 3)
+rownames(tE) <- c("Centro", "[0,10]km dal centro", "-10 km dal centro")
+colnames(tE) <- c("Alquilar", "Facturas", "Transporte")
+tE
+
+criterio.Todos(tE, alfa=0.3,favorable = FALSE)
+
+dibuja.criterio.Hurwicz(tE,favorable=FALSE)
+alfas(tE,precisionAlfa = 0.02,favorable = FALSE)
+alfas(tE,precisionAlfa = 0.07,favorable = FALSE)
